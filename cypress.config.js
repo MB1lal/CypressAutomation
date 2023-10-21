d0c4db4ea1ea2516de9ab5249f6175fe7e8b2a3f
Bilal <m.b1lal@hotmail.com>
2023-10-21 23:13:17 +0400

    Implemented reporting

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,13 @@ const { defineConfig } = require("cypress");
 const {downloadFile} = require('cypress-downloadfile/lib/addPlugin');
 
 module.exports = defineConfig({
+   reporter: 'cypress-mochawesome-reporter',
    e2e: {
       baseUrl: 'https://the-internet.herokuapp.com',
       defaultCommandTimeout: 10000,
       setupNodeEvents(on, config) {
          on('task', {downloadFile});
+         require('cypress-mochawesome-reporter/plugin')(on);
        },
     },
 });
